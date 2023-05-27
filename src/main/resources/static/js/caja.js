f01b75641d90dc2fcd0c66b131fd9a19f5138f81
AnthonyJorge <jorgito09.82chavez@gmail.com>
2023-05-27 01:05:15 -0500

    caja terminada

---
+++ src/main/resources/static/js/caja.js
@@ -0,0 +1,94 @@
+
+import { showModal } from "./modal.js";
+
+const $d = document;
+
+$d.addEventListener("DOMContentLoaded", () => {
+  initializeTable();
+  addEventToTable();
+  addEventToButtonAdd();
+  addEventToButtonConfirmAddAndConfirmUpdate();
+});
+
+const initializeTable = () => {
+  const table = $("#tablaCaja").DataTable({
+    language: {
+      url: "/language/datatables-es-mx.json",
+    },
+    responsive: true,
+    fixedHeader: true,
+    rowId: "0",
+    columns: [
+      null,
+      { orderable: false, searchable: false, width: 50 },
+    ],
+    initComplete: function () {
+      $("#tablaCaja thead tr th").each(function (i) {
+        const title = $(this).text();
+
+        if (
+          !["Eliminar"].includes(title))
+        {
+          $("input", this).on("keyup change", function () {
+            if (table.column(i).search() !== this.value) {
+              table.column(i).search(this.value).draw();
+            }
+          });
+        }
+      });
+    },
+  });
+};
+
+const addEventToTable = () => {
+  $("#tablaCaja tbody").on(
+    "click",
+    "td .icon-delete",
+    function (e) {
+      let id;
+
+      const 
+        $listBtnDelete = $(".icon-delete");
+
+      if ($(this).parents("tr").hasClass("child")) {
+        id = $(this).parents("tr").prev().find("td:eq(0)").text();
+      } else {
+        id = $(this).closest("tr").find("td:eq(0)").text();
+      }
+
+
+      if ($listBtnDelete.filter(e.currentTarget).length) {
+        const contentModal = {
+          header: `<i class="icon text-center text-danger bi bi-trash-fill"></i>
+						<h6
+						 class="modal-title text-center" id="modal-prototype-label">¿ESTÁS SEGURO DE ELIMINAR CAJA: ${id}?</h6>`,
+          body: `<form id="form-delete" action="/configuracion/caja/eliminar" method="POST">
+							<input type="hidden" name="id" value="${id}"/>
+						</form>`,
+          footer: `<input form="form-delete" type="submit" class="w-50 text-white btn btn-danger" value="ELIMINAR"/>
+						<button data-bs-dismiss="modal" aria-label="Close" class="w-50 btn btn-primary">CANCELAR</button>`,
+        };
+
+        showModal(contentModal);
+      }
+    }
+  );
+};
+
+const addEventToButtonAdd = () => {
+  $("#btn-add").on("click", () => {
+    const contentModal = {
+      header: `<i class="icon text-center text-primary bi bi-plus-circle-fill"></i>
+						<h4 class="modal-title text-center" id="modal-prototype-label">Añadir Caja</h4>`,
+	  body:  `<form class="d-flex flex-column gap-4" id="form-add" action="/configuracion/caja/grabar" method="POST">		
+							<div class=" modal-title text-center">
+								<label for="name">¿Seguro que deseas agregar una caja?</label>	
+							</div>
+						</form>`,
+      footer: `<input id="add" form="form-add" type="submit" class="w-50 btn btn-primary btn-lg" value="AÑADIR"/>
+						<button data-bs-dismiss="modal" aria-label="Close" class="w-50 btn btn-primary btn-lg">CANCELAR</button>`,
+    };
+
+    showModal(contentModal);
+  });
+};
